7b256ae80b9dccef4c55a9b5045a8a6374dd5c74
Igor Povar <goshuchok@gmail.com>
2024-08-17 19:56:39 +0300

    fix bugs

--- app/breed/[type]/[id]/page.tsx
+++ app/breed/[type]/[id]/page.tsx
@@ -1,10 +1,7 @@
-"use client"
-
-import { useEffect, useState } from 'react';
 import { fetchBreedDetails } from '../../../utils/api';
 import Image from 'next/image';
 
-type BreedDetails = {
+type Breed = {
   name: string;
   weight: string;
   height: string;
@@ -14,44 +11,38 @@ type BreedDetails = {
   bred_for: string;
 };
 
-export default function BreedDetail({ params }: { params: { type: string; id: number } }) {
+type BreedDetails = {
+  breeds: Breed[] | null;
+  images: {
+    url: string;
+  } | null;
+};
+
+type BreedDetailProps = {
+  params: {
+    type: string;
+    id: number;
+  };
+};
+
+export default async function BreedDetail({ params }: BreedDetailProps) {
   const { type, id } = params;
-  const [breedDetails, setBreedDetails] = useState<{ breed: BreedDetails | null, image: string | null } | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (id) {
-      const fetchData = async () => {
-        try {
-          const data = await fetchBreedDetails(type, id);
-          setBreedDetails(data);
-          setLoading(false);
-        } catch (error) {
-          console.error('Failed to fetch breed details:', error);
-          setLoading(false);
-        }
-      };
-      fetchData();
-    }
-  }, [type, id]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const breedDetails: BreedDetails | null = await fetchBreedDetails(type, id);
 
   if (!breedDetails) {
     return <div>No details found for this breed.</div>;
   }
 
-  const { breed, image } = breedDetails;
+  const { breeds, images } = breedDetails;
+  const breed = breeds?.[0];
 
   return (
     <div className="min-h-screen p-8 bg-gray-100">
       <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold mb-6">{breed?.name}</h1>
-        {image && (
+        <h1 className="text-2xl font-bold mb-6 text-black">Name: {breed?.name}</h1>
+        {images && (
           <Image 
-            src={image} 
+            src={images.url} 
             alt={breed ? breed.name : 'Unknown Breed'}
             width={400} 
             height={200} 
@@ -59,17 +50,12 @@ export default function BreedDetail({ params }: { params: { type: string; id: nu
             unoptimized 
           />
         )}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 gap-6">
           <p className="text-lg font-semibold text-black">Life Span: {breed?.life_span}</p>
           <p className="text-lg font-semibold text-black">Temperament: {breed?.temperament}</p>
-          {/* <p className="text-lg font-semibold text-black">Bred For: {breed?.bred_for}</p> */}
+          <p className="text-lg font-semibold text-black">Bred For: {breed?.bred_for || 'Unknown'}</p>
         </div>
       </div>
     </div>
   );
 }
-
-
-
-
-
--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,37 +1,38 @@
 const DOG_API_URL = 'https://api.thedogapi.com/v1';
 const CAT_API_URL = 'https://api.thecatapi.com/v1';
+const REACT_APP_CAT_API_KEY ="live_Ct4aOOsBuQF9uGI7QDFWJAVa2zt788srmv3qcKWZKLkmTEjuVW9Yf5U8DRT1UbZP";
+const REACT_APP_DOG_API_KEY ="live_awFiHKxtZPBTv2Brdb0vuhX5TWzrFFgKA7Jk0N6n6duHaVSqMfvbp68ZZn6mdhuN";
 
 export const fetchDogBreeds = async () => {
-    const response = await fetch(`${DOG_API_URL}/breeds`);
-    const breedsDog = await response.json();
-
-    const imageResponse = await fetch(`${DOG_API_URL}/images/search?limit=10`);
-    const imagesDog = await imageResponse.json();
-
-    return { breedsDog, imagesDog }; 
+     const response = await fetch(`${DOG_API_URL}/images/search?limit=21&has_breeds=1`, {
+        headers: {
+            "content-type": "application/json",
+            'x-api-key': REACT_APP_DOG_API_KEY,
+        }
+    }); 
+    const json = await response.json();
+    return { breedsDog: json, imagesDog: json }; 
 };
 
 export const fetchCatBreeds = async () => {
-    const response = await fetch(`${CAT_API_URL}/breeds`);
-    const breeds = await response.json();    
-    
-    const imageResponse = await fetch(`${CAT_API_URL}/images/search?limit=10`);
-    const images = await imageResponse.json();
-    
-    return { breeds, images }; 
+    const response = await fetch(`${CAT_API_URL}/images/search?limit=21&has_breeds=1`, {
+        headers: {
+            "content-type": "application/json",
+            'x-api-key': REACT_APP_CAT_API_KEY,
+        }
+    });
+    const json = await response.json();
+    return { breeds: json, images: json };
 };
 
 export const fetchBreedDetails = async (type: string, id: number) => {
-    const apiUrl = type === 'dog' ? DOG_API_URL : CAT_API_URL;
-    const response = await fetch(`${apiUrl}/breeds/${id}`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch breed details');
-    }
-    const breedDetails = await response.json();
-    const imageResponse = await fetch(`${apiUrl}/images/search?breed_id=${id}&limit=1`);
-    const images = await imageResponse.json();
-    return {
-        breed: breedDetails,
-        image: images.length > 0 ? images[0].url : null,
-    };
+    const apiUrl = type === 'dog' ? DOG_API_URL : CAT_API_URL; 
+    const response = await fetch(`${apiUrl}/images/${id}`, {
+        headers: {
+            "content-type": "application/json",
+            'x-api-key': REACT_APP_CAT_API_KEY,
+        }
+    });
+    const json = await response.json();
+    return { breeds: json.breeds, images: { url: json.url, width: json.width, height: json.height } };
 };
